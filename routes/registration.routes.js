a9335dbe408ab8ae16f702b942f27b2bfed4637e
Nasir-afridi <nasirwc05@gmail.com>
2025-01-29 18:34:21 +0500

    Added the registration routes(login, signup) in this file

---
+++ routes/registration.routes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import {Router} from 'express';
+import { Signup,Login } from '../controllers/registration.controllers.js';
+
+
+const router = new Router();
+router.post('/signup',Signup);
+router.post('/login', Login);
+
+export default router;

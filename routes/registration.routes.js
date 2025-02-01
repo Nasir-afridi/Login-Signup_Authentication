2a956b389e70ce4308861bd58e09f23f25b77b92
Nasir-afridi <nasirwc05@gmail.com>
2025-02-01 19:02:52 +0500

    Added the registration-router file

--- routes/registration.routes.js
+++ routes/registration.routes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import {Router} from 'express';
 import { Signup,Login } from '../controllers/registration.controllers.js';
 
-
 const router = new Router();
 router.post('/signup',Signup);
 router.post('/login', Login);
